86af7f3b2e5de6f1e449db7b555297a6f11be57d
k.langer@eloaded.eu <k.langer@eloaded.eu>
2024-05-24 18:53:09 +0200

    Relax prettier width, improve LOC for send

--- src/connector/connector.service.ts
+++ src/connector/connector.service.ts
@@ -66,27 +66,31 @@ export class ConnectorService {
 
   /**
    * Sends a request to the specified endpoint with the provided data.
+   * Retries if the request fails as often as specified on the environment.
    * @param endpoint The endpoint to send the request to.
    * @param data The data to send with the request.
    * @returns An Observable that emits the AxiosResponse object.
    * @throws An error if the request fails.
    */
   async send(endpoint: string, data: any): Promise<AxiosResponse | undefined> {
-    try {
-      const response = await this.httpService.post(endpoint, data).toPromise();
-      if (!response) {
-        throw new Error(`Request to ${endpoint} failed`);
+    const retryCount = this.configService.getCurrentVariableValue("RETRY_COUNT", 3);
+    let attempts = 0;
+    do {
+      try {
+        const response = await this.httpService.post(endpoint, data).toPromise();
+        if (!response || response.status < 200 || response.status > 299) {
+          throw new Error(`Request to ${endpoint} failed with status ${response?.status}`);
+        }
+        return response;
+      } catch (error) {
+        this.logger.error(`Error sending request to ${endpoint}: ${JSON.stringify(error)}`);
+        attempts++;
+        // <= since first attempt is not a retry
+        if (attempts <= retryCount) {
+          this.logger.log(`Retrying request to ${endpoint} [${attempts}/${retryCount}]`);
+          await new Promise((resolve) => setTimeout(resolve, 5000));
+        }
       }
-      if (response.status < 200 || response.status > 299) {
-        this.logger.error(
-          `Request to ${endpoint} failed with status ${response.status}`,
-        );
-      }
-      return response;
-    } catch (error) {
-      this.logger.error(
-        `Error sending request to ${endpoint}: ${JSON.stringify(error)}`,
-      );
-    }
+    } while (attempts <= retryCount);
   }
 }
