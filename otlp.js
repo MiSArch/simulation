c966e3e40793f689056a5d689d030fe9325d46e4
Elias Müller <elias_benedikt.mueller@porsche.de>
2025-06-07 11:04:30 +0200

    chore: remove tracer from node otel builds

--- otlp.js
+++ otlp.js
@@ -1,11 +1,9 @@
 const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
 const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-http');
 const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 
 const sdk = new NodeSDK({
-  traceExporter: new OTLPTraceExporter({ url: 'http://otel-collector:4318/v1/traces' }),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter({ url: 'http://otel-collector:4318/v1/metrics' }),
     exportIntervalMillis: 5000,
