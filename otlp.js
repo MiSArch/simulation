87f244b47cb7dd7a8d81b821968a4c11341387b1
eliasmueller <elias@mueller-kleinheinz.de>
2025-06-29 14:44:26 +0200

    chore: make otlp endpoint configurable

--- otlp.js
+++ otlp.js
@@ -2,10 +2,11 @@ const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-http');
 const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const otlpEndpoint = process.env.OTEL_EXPORTER_OTLP_ENDPOINT || 'http://otel-collector:4318';
 
 const sdk = new NodeSDK({
   metricReader: new PeriodicExportingMetricReader({
-    exporter: new OTLPMetricExporter({ url: 'http://otel-collector:4318/v1/metrics' }),
+    exporter: new OTLPMetricExporter({ url: `${otlpEndpoint}/v1/metrics` }),
     exportIntervalMillis: 5000,
   }),
   instrumentations: [
